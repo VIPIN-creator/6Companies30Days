42433c757043052d1543735986c64345757ed754
Vipin  Tripathi <59695863+VIPIN-creator@users.noreply.github.com>
2022-01-02 21:13:00 +0530

    Update Q7.cpp

--- Goldman Sachs/Day 2/Q7.cpp
+++ Goldman Sachs/Day 2/Q7.cpp
@@ -1,1 +1,9 @@
 // https://www.geeksforgeeks.org/distributing-m-items-circle-size-n-starting-k-th-position/
+
+// Sol 1:- https://practice.geeksforgeeks.org/viewSol.php?subId=32f74be0eb5b56993b22df930ed85fe8&pid=704216&user=2019kucp1053
+//  time:- o(1)
+
+int findPosition(int N , int M , int K) {
+        // code here
+        return (K-1 + M)%N == 0 ? N :  (K-1 + M)%N;
+    }
