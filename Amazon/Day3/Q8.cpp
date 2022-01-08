22c99defb928677fb1a6012152a053a22615c374
Vipin  Tripathi <59695863+VIPIN-creator@users.noreply.github.com>
2022-01-08 11:25:43 +0530

    Create Q8.cpp

---
+++ Amazon/Day3/Q8.cpp
@@ -0,0 +1,6 @@
+// https://practice.geeksforgeeks.org/problems/count-ways-to-nth-stairorder-does-not-matter1322/1/
+long long countWays(int m)
+    {
+        // your code here
+        return m/2 + 1;
+    }
